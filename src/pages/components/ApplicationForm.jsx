69cd214b1531add00241d65e05fd917f52251305
kahasan <kahraman.h@outlook.com>
2021-08-18 21:25:42 +0300

    fix: update form element

---
+++ src/pages/components/ApplicationForm.jsx
@@ -0,0 +1,40 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-alert */
+import React from 'react';
+
+import { Flex } from '@chakra-ui/react';
+
+import { useForm } from 'react-hook-form';
+
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import FormInput from './FormInput';
+import FormTextArea from './FormTextArea';
+
+const schema = yup.object().shape({
+  firstName: yup.string().required(),
+  lastName: yup.string().required(),
+  year: yup.number().min(1920).max(2003).required(),
+  address: yup.string().required(),
+});
+
+export default function ApplicationForm() {
+  const { register, handleSubmit, formState: { errors } } = useForm({
+    resolver: yupResolver(schema),
+  });
+  const onSubmit = (data) => {
+    alert(JSON.stringify(data));
+  };
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Flex flexDir="column">
+        <FormInput label="firstName" inputTitle="First Name" register={register} errors={errors} required />
+        <FormInput label="lastName" inputTitle="Last Name" register={register} errors={errors} required />
+        <FormInput label="year" inputTitle="Year of Birth" register={register} errors={errors} required />
+        <FormTextArea label="address" inputTitle="Address" register={register} errors={errors} required />
+        <button type="submit">send</button>
+      </Flex>
+    </form>
+
+  );
+}
---
+++ src/pages/components/FormInput.jsx
@@ -0,0 +1,39 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import {
+  Input,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+} from '@chakra-ui/react';
+
+export default function FormInput({
+  inputTitle, label, register, required, errors,
+}) {
+  return (
+    <>
+      <FormControl isInvalid={errors[`${label}`]}>
+        <FormLabel htmlFor={label}>{inputTitle}</FormLabel>
+        <Input {...register(label, { required })} borderColor="black" borderRadius="lg" mt="3" id={label} />
+        <FormErrorMessage>{errors[`${label}`] && errors[`${label}`].message}</FormErrorMessage>
+      </FormControl>
+    </>
+  );
+}
+
+FormInput.defaultProps = {
+  required: false,
+  label: '',
+  inputTitle: '',
+  errors: {},
+};
+
+FormInput.propTypes = {
+  label: PropTypes.string,
+  inputTitle: PropTypes.string,
+  register: PropTypes.func.isRequired,
+  required: PropTypes.bool,
+  errors: PropTypes.shape({}),
+};
