69a657d7decfe6723fb594de8d3334ac6092fcaf
kahasan <kahraman.h@outlook.com>
2021-08-18 15:44:53 +0300

    feat: edit firebase config

---
+++ src/services/firebase.js
@@ -0,0 +1,23 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FB_API_KEY,
+  authDomain: process.env.REACT_APP_FB_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FB_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FB_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FB_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FB_APP_ID,
+  measurementId: process.env.REACT_APP_FB_MEASUREMENT_ID,
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+} else {
+  // if already initialized, use that one
+  firebase.app();
+}
+firebase.analytics();
+
+const db = firebase.firestore();
+
+export default db;
