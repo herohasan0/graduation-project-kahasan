b0177bbfdce3341544dee7f4abf66d3f28215058
kahasan <kahraman.h@outlook.com>
2021-08-19 14:48:19 +0300

    fix: edit layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,12 +4,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Cover from 'components/Cover';
 
-import { Flex } from '@chakra-ui/react';
+import { Flex, Image, Box } from '@chakra-ui/react';
 
-export default function Layout({ imgUrl, children }) {
+export default function Layout({
+  imgUrl, children, title, helperText,
+}) {
   return (
     <Flex>
-      <Flex w="half">{children}</Flex>
+      <Flex w="half" px={32.5} flexDir="column" mt={32.5} mb={48}>
+        <Flex mb={5} flexDir="column">
+          <Image src="./assets/images/logo.png" w="3xs" />
+          <Box mt={10}>
+            {title}
+          </Box>
+          <Box mt={10}>
+            {helperText}
+          </Box>
+        </Flex>
+        <Flex>{children}</Flex>
+      </Flex>
       <Flex w="half">
         <Cover imgUrl={imgUrl} />
       </Flex>
