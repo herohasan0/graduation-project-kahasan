f3c14f8e6004c6215741ecdfa74003240e88a7ea
kahasan <kahraman.h@outlook.com>
2021-08-21 19:24:20 +0300

    feat: add layout for admin pages

---
+++ src/components/AdminLayout.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+
+import { Flex } from '@chakra-ui/react';
+
+import Sidebar from 'components/Sidebar';
+import SideBarItem from 'pages/components/SideBarItem';
+
+import { sidebarMinWidth, sidebarMaxWidth } from 'CONSTANS';
+
+export default function AdminLayout() {
+  const [width, setWidth] = useState(sidebarMaxWidth);
+  const handleWidth = () => setWidth(width === sidebarMaxWidth ? sidebarMinWidth : sidebarMaxWidth);
+  return (
+    <Flex>
+      <Sidebar navTitle="Admin." handleWidth={handleWidth} width={width}>
+        <SideBarItem width={width} route="/forms" />
+      </Sidebar>
+    </Flex>
+  );
+}
