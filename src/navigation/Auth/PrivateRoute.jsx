b1a4872021cba9315e9391eeddc9af0f13ea3a75
kahasan <kahraman.h@outlook.com>
2021-08-21 12:53:00 +0300

    feat: create private route

---
+++ src/navigation/Auth/PrivateRoute.jsx
@@ -0,0 +1,29 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+import { Redirect, Route } from 'react-router-dom';
+import firebase from 'services/firebase';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+
+function PrivateRoute({ children, ...rest }) {
+  const [user] = useAuthState(firebase.auth());
+  return (
+
+    <Route
+      {...rest}
+      render={() => (user ? (
+        <>
+          {children}
+        </>
+      ) : (
+        <Redirect
+          to="/admin"
+        />
+      )
+      )}
+    />
+
+  );
+}
+export default PrivateRoute;
