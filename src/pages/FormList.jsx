47beb91b1aa3bd6cb8a91d0d3e71ed0a29b0fc77
kahasan <kahraman.h@outlook.com>
2021-08-24 13:23:59 +0300

    fix: edit Form List

--- src/pages/FormList.jsx
+++ src/pages/FormList.jsx
@@ -2,18 +2,21 @@ import React from 'react';
 import ListItem from 'components/Layout/components/ListItem';
 import { getForms } from 'services/firestore';
 import ConvertDate from 'helper/ConvertDate';
+import { Flex } from '@chakra-ui/react';
 
 export default function FormList() {
   const forms = getForms();
 
   return (
     <>
-      {
+      <Flex flexDir="column" h="24.375rem" overflowY="scroll" mb="2.5">
+        {
         forms && forms.map(({
           id, firstName, subject, planetOfBirth, status, createdDate,
         }) => (
           <ListItem
             key={id}
+            id={id}
             applyBy={firstName}
             subject={subject}
             from={planetOfBirth}
@@ -22,6 +25,7 @@ export default function FormList() {
           />
         ))
       }
+      </Flex>
     </>
   );
 }
