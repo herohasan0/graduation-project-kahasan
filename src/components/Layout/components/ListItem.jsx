88430aa6feac67a7dd5970353412977aa034df1b
kahasan <kahraman.h@outlook.com>
2021-08-24 14:33:33 +0300

    feat: add menu component to list item

--- src/components/Layout/components/ListItem.jsx
+++ src/components/Layout/components/ListItem.jsx
@@ -1,45 +1,54 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 
 import {
-  Flex, Divider, Text, Image, Center,
+  Flex, Divider, Text, Image, Center, Menu, MenuButton, MenuList, MenuItem,
 } from '@chakra-ui/react';
 
-import PropTypes from 'prop-types';
+import ConvertDate from 'helper/ConvertDate';
 
+import { useHistory } from 'react-router-dom';
 import StatusBar from './StatusBar';
 
-export default function ListItem({
-  avatarSrc, applyBy, subject, from, status, createdDate,
-}) {
+export default function ListItem({ form }) {
+  const history = useHistory();
+  const {
+    firstName, subject, planetOfBirth, status, createdDate, id,
+  } = form;
   return (
     <Flex flexDir="column" pt="4" fontSize="sm">
       <Flex alignItems="center" justifyContent="space-between">
         <Flex alignItems="center">
-          <Image borderRadius="full" w="imageMdx" h="imageMdx" src={avatarSrc} />
-          <Text ml="1.5">{applyBy}</Text>
+          <Image borderRadius="full" w="imageMdx" h="imageMdx" src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" />
+          <Text ml="1.5">{firstName}</Text>
         </Flex>
         <Text>{subject}</Text>
-        <Text>{from}</Text>
+        <Text>{planetOfBirth}</Text>
 
         <StatusBar status={status} />
 
-        <Text>{createdDate}</Text>
-        <Center><Image src="/assets/icons/action.png" w="imageMd" h="imageMd" /></Center>
+        <Text>{ConvertDate(createdDate.seconds)}</Text>
+
+        <Menu>
+          <MenuButton as={Center} cursor="pointer">
+            <Image src="/assets/icons/action.png" w="imageMd" h="imageMd" />
+          </MenuButton>
+          <MenuList>
+            <MenuItem onClick={() => {
+              history.push({
+                pathname: `/admin/basvuru-listesi/${id}`,
+                state: { result: form },
+              });
+            }}
+            >
+              Edit
+
+            </MenuItem>
+          </MenuList>
+        </Menu>
+
       </Flex>
       <Divider mt="4" />
     </Flex>
   );
 }
-
-ListItem.defaultProps = {
-  avatarSrc: 'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
-};
-
-ListItem.propTypes = {
-  avatarSrc: PropTypes.string,
-  applyBy: PropTypes.string.isRequired,
-  subject: PropTypes.string.isRequired,
-  from: PropTypes.string.isRequired,
-  status: PropTypes.string.isRequired,
-  createdDate: PropTypes.string.isRequired,
-};
