94372c402d2c06fbd0d69b0a7149c411c5f86f28
kahasan <kahraman.h@outlook.com>
2021-08-24 23:58:31 +0300

    fix: add constant width to element of component

--- src/components/Layout/components/ListItem.jsx
+++ src/components/Layout/components/ListItem.jsx
@@ -17,17 +17,19 @@ export default function ListItem({ form }) {
   } = form;
   return (
     <Flex flexDir="column" pt="4" fontSize="sm">
-      <Flex alignItems="center" justifyContent="space-between">
-        <Flex alignItems="center">
+      <Flex alignItems="center">
+        <Flex alignItems="center" w="227px">
           <Image borderRadius="full" w="imageMdx" h="imageMdx" src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" />
           <Text ml="1.5">{firstName}</Text>
         </Flex>
-        <Text>{subject}</Text>
-        <Text>{planetOfBirth}</Text>
+        <Text w="275px">{subject}</Text>
+        <Text w="166px">{planetOfBirth}</Text>
 
-        <StatusBar status={status} />
+        <Flex w="216px">
+          <StatusBar status={status} />
+        </Flex>
 
-        <Text>{ConvertDate(createdDate.seconds)}</Text>
+        <Text w="193px">{ConvertDate(createdDate.seconds)}</Text>
 
         <Menu>
           <MenuButton as={Center} cursor="pointer">
