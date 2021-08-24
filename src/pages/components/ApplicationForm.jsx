54cf21018322c8237d4b92fe3242830144efde43
kahasan <kahraman.h@outlook.com>
2021-08-24 23:38:51 +0300

    feat: add admin notes to check form

--- src/pages/components/ApplicationForm.jsx
+++ src/pages/components/ApplicationForm.jsx
@@ -5,7 +5,7 @@
 import React, { useState } from 'react';
 
 import {
-  Flex, Button, Box, Badge,
+  Flex, Button, Box, Badge, Text, Alert, AlertIcon, AlertTitle, AlertDescription,
 } from '@chakra-ui/react';
 
 import { useHistory } from 'react-router-dom';
@@ -35,6 +35,8 @@ export default function ApplicationForm({ formValue, disabled }) {
     resolver: yupResolver(schema),
   });
 
+  console.log('formValue****', formValue);
+
   const history = useHistory();
 
   const onSubmit = async (data) => {
@@ -54,30 +56,70 @@ export default function ApplicationForm({ formValue, disabled }) {
     });
   };
 
+  const statusVariant = {
+    Approved: {
+      colorScheme: 'green',
+      status: 'success',
+      title: 'Congratulations! :)',
+    },
+    Cancelled: {
+      colorScheme: 'red',
+      status: 'error',
+      title: 'Soory :(',
+    },
+    Pending: {
+      colorScheme: 'gray',
+    },
+  };
+
   return (
     <Box w="full">
       <form onSubmit={handleSubmit(onSubmit)}>
-        {formValue && formValue.status && formValue.status && (
+        {formValue && formValue.status && (
           <>
             Application status:
-            <Badge colorScheme="green">
+            <Badge colorScheme={statusVariant[formValue.status].colorScheme}>
               {formValue.status}
             </Badge>
           </>
         )}
+
+        {formValue && formValue.adminNoted && (
+        <>
+          <Text>Admin Note:</Text>
+          <Alert
+            status={statusVariant[formValue.status].status}
+            variant="subtle"
+            flexDirection="column"
+            alignItems="center"
+            justifyContent="center"
+            textAlign="center"
+            py="30px"
+          >
+            <AlertIcon boxSize="40px" mr={0} />
+            <AlertTitle mt={4} mb={1} fontSize="lg">
+              {statusVariant[formValue.status].title}
+            </AlertTitle>
+            <AlertDescription maxWidth="sm">
+              {formValue.adminNoted}
+            </AlertDescription>
+          </Alert>
+        </>
+        )}
+
         <Flex flexDir="column">
           <FormInput label="firstName" inputTitle="First Name" register={register} errors={errors} required disabled={disabled} value={formValue && formValue.firstName} />
           <FormInput label="lastName" inputTitle="Last Name" register={register} errors={errors} required disabled={disabled} value={formValue && formValue.lastName} />
           <FormInput label="yearOfBirth" inputTitle="Year of Birth" register={register} errors={errors} required disabled={disabled} value={formValue && formValue.yearOfBirth} />
           <FormTextArea label="address" inputTitle="Address" register={register} errors={errors} required disabled={disabled} value={formValue && formValue.address} />
           <FormSelect label="planetOfBirth" inputTitle="Planet of Birth" register={register} errors={errors} required disabled={disabled} value={formValue && formValue.planetOfBirth} placeholder="Select a planet">
-            <option>Magrathea</option>
-            <option>Earth</option>
-            <option>NowWhat</option>
-            <option>Bethselamin</option>
-            <option>Damogran</option>
-            <option>Betelgeuse 5</option>
-            <option>Viltvodle 6</option>
+            <option value="Magrathea">Magrathea</option>
+            <option value="Earth">Earth</option>
+            <option value="NowWhat">NowWhat</option>
+            <option value="Bethselamin">Bethselamin</option>
+            <option value="Damogran">Damogran</option>
+            <option value="Betelgeuse 5">Betelgeuse 5</option>
+            <option value="Viltvodle 6">Viltvodle 6</option>
           </FormSelect>
 
           {!disabled
