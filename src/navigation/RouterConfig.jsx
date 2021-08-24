8ab6164fcb4d329e4ae54135a25fa832c66e1493
kahasan <kahraman.h@outlook.com>
2021-08-25 00:55:59 +0300

    feat: add list of form path to private route

--- src/navigation/RouterConfig.jsx
+++ src/navigation/RouterConfig.jsx
@@ -1,8 +1,11 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, useHistory } from 'react-router-dom';
 import FormPage from 'pages/FormPage';
 import SuccessPage from 'pages/SuccessPage';
+import {
+  Divider, Flex, Box, Button,
+} from '@chakra-ui/react';
 import {
   ROOT, FORM, SUCCESS, CHECK, CHECK_SPECIFIC, ADMIN, LIST,
 } from 'navigation/CONSTANTS';
@@ -18,70 +21,93 @@ import CheckStatus from 'pages/CheckStatus';
 import CheckStatusResult from 'pages/CheckStatusResult';
 import AdminLayout from 'components/Layout/AdminLayout';
 import FormList from 'pages/FormList';
+import ListHeader from 'components/Layout/components/ListHeader';
+import FormTextArea from 'pages/components/FormTextArea';
+import AdminResponseForm from 'pages/components/AdminResponseForm';
 import Admin from './Auth/Admin';
 import PrivateRoute from './Auth/PrivateRoute';
 
-const RouterConfig = () => (
-  <Switch>
-    {/* List all public routes here */}
-    <Route exact path={ROOT}>
-      <Layout
-        imgUrl={COVERURL}
-      >
-        <Home />
-      </Layout>
-    </Route>
+const RouterConfig = () => {
+  const history = useHistory();
 
-    <Route exact path={FORM}>
-      <Layout
-        helperText={<HelperText text={HELPERTEXT} />}
-        title={<Title title={FORMTITLE} />}
-        imgUrl={COVERURL}
-      >
-        <FormPage />
-      </Layout>
-    </Route>
+  return (
+    <Switch>
+      {/* List all public routes here */}
+      <Route exact path={ROOT}>
+        <Layout
+          imgUrl={COVERURL}
+        >
+          <Home />
+        </Layout>
+      </Route>
 
-    <Route exact path={SUCCESS}>
-      <Layout imgUrl={COVERURL}>
-        <SuccessPage />
-      </Layout>
-    </Route>
+      <Route exact path={FORM}>
+        <Layout
+          helperText={<HelperText text={HELPERTEXT} />}
+          title={<Title title={FORMTITLE} />}
+          imgUrl={COVERURL}
+        >
+          <FormPage />
+        </Layout>
+      </Route>
 
-    <Route exact path={CHECK}>
-      <Layout
-        imgUrl={COVERURL}
-        title={<Title title={CHECKTITLE} />}
-        helperText={<HelperText text={CHECKDESCRIPTION} />}
-      >
-        <CheckStatus />
-      </Layout>
-    </Route>
+      <Route exact path={SUCCESS}>
+        <Layout imgUrl={COVERURL}>
+          <SuccessPage />
+        </Layout>
+      </Route>
 
-    <Route exact path={CHECK_SPECIFIC}>
-      <Layout
-        imgUrl={COVERURL}
-        title={<Title title={CHECKTITLE} />}
-      >
-        <CheckStatusResult />
-      </Layout>
-    </Route>
+      <Route exact path={CHECK}>
+        <Layout
+          imgUrl={COVERURL}
+          title={<Title title={CHECKTITLE} />}
+          helperText={<HelperText text={CHECKDESCRIPTION} />}
+        >
+          <CheckStatus />
+        </Layout>
+      </Route>
 
-    <Route exact path={ADMIN}>
-      <Admin />
-    </Route>
+      <Route exact path={CHECK_SPECIFIC}>
+        <Layout
+          imgUrl={COVERURL}
+          title={<Title title={CHECKTITLE} />}
+        >
+          <CheckStatusResult />
+        </Layout>
+      </Route>
 
-    <PrivateRoute exact path={LIST}>
-      <AdminLayout>
-        <FormList />
-      </AdminLayout>
-    </PrivateRoute>
+      <Route exact path={ADMIN}>
+        <Admin />
+      </Route>
 
-    {/* List a generic 404-Not Found route here */}
-    <Route path="*">
-      <NotFound />
-    </Route>
-  </Switch>
-);
+      <PrivateRoute exact path={LIST}>
+        <AdminLayout>
+          <ListHeader />
+          <Divider mt="2.5" />
+          <FormList />
+        </AdminLayout>
+      </PrivateRoute>
+
+      <PrivateRoute exact path="/admin/basvuru-listesi/:id">
+        <AdminLayout>
+          <Flex cursor="pointer" w="48px" h="48px" onClick={() => history.push('/admin/basvuru-listesi')}>&lt;</Flex>
+          <Divider mt="2.5" mb="2.5" />
+          <Box overflowY="scroll" h="395px">
+            <CheckStatusResult />
+            <Divider mt="25px" mb="2.5" />
+            <AdminResponseForm />
+
+          </Box>
+
+        </AdminLayout>
+      </PrivateRoute>
+
+      {/* List a generic 404-Not Found route here */}
+      <Route path="*">
+        <NotFound />
+      </Route>
+    </Switch>
+  );
+};
 
 export default RouterConfig;
