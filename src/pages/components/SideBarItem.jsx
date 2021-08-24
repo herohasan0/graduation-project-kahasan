a6572bbe3886fcc8553df6932cb2f73e8e60878a
kahasan <kahraman.h@outlook.com>
2021-08-24 08:56:25 +0300

    fix: update title text

--- src/pages/components/SideBarItem.jsx
+++ src/pages/components/SideBarItem.jsx
@@ -8,13 +8,15 @@ import { sidebarMinWidth } from 'CONSTANS';
 
 import PropTypes from 'prop-types';
 
-export default function SideBarItem({ width, route, imgSrc }) {
+export default function SideBarItem({
+  title, width, route, imgSrc,
+}) {
   return (
     <Link to={route}>
       <Flex alignItems="center" h="48px" _hover={{ backgroundColor: 'hover.black' }} py={3} pl="31px">
         <Image w="21px" h="21px" src={imgSrc} />
         {width > sidebarMinWidth && (
-        <Text transition="0.5s" ml="5px">Forms</Text>
+        <Text transition="0.5s" ml="5px">{title}</Text>
         )}
       </Flex>
     </Link>
@@ -22,6 +24,7 @@ export default function SideBarItem({ width, route, imgSrc }) {
 }
 
 SideBarItem.propTypes = {
+  title: PropTypes.string.isRequired,
   width: PropTypes.number.isRequired,
   route: PropTypes.string.isRequired,
   imgSrc: PropTypes.string.isRequired,
