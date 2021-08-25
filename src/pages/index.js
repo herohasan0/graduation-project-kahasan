53195a7a2a9b59621d254df46667aac47f155dd6
kahasan <kahraman.h@outlook.com>
2021-08-25 19:21:53 +0300

    feat: create export file in pages

---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+export * from './CheckStatus';
+export * from './CheckStatusResult';
+export * from './FormList';
+export * from './FormPage';
+export * from './SuccessPage';
+export * from './Home';
