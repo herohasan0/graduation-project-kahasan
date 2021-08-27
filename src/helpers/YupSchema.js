0cbf8bc0acf0fe21d2bd0341f02c444ec71063bf
kahasan <kahraman.h@outlook.com>
2021-08-27 19:52:21 +0300

    feat: create new yup schema

--- src/helpers/YupSchema.js
+++ src/helpers/YupSchema.js
@@ -1,6 +1,6 @@
 import * as yup from 'yup';
 
-export const schema = yup.object().shape({
+export const applicationFormSchema = yup.object().shape({
   firstName: yup.string().max(50).required(),
   lastName: yup.string().max(50).required(),
   yearOfBirth: yup.number().required(),
@@ -8,4 +8,18 @@ export const schema = yup.object().shape({
   planetOfBirth: yup.string().required(),
   address: yup.string().max(250).required(),
   reasonOfApply: yup.string().max(350).required(),
+});
+
+export const adminResponseFormSchema = yup.object().shape({
+  response: yup.string(),
+  status: yup.string().required(),
+});
+
+export const loginFormSchema = yup.object().shape({
+  email: yup.string().required(),
+  password: yup.string().required(),
+});
+
+export const checkStatusFormSchema = yup.object().shape({
+  formId: yup.string().required(),
 });
