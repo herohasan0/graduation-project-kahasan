a933ccd862a6de2fe8f242000d7684c79ac009e5
kahasan <kahraman.h@outlook.com>
2021-08-27 19:50:32 +0300

    feat: create form context component warp app with it

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,13 +5,16 @@ import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import RouterConfig from 'navigation/RouterConfig';
 import { AuthProvider } from 'contexts/authContext';
+import { FormProvider } from 'contexts/formContext';
 
 export function App() {
   return (
     <AuthProvider>
-      <BrowserRouter>
-        <RouterConfig />
-      </BrowserRouter>
+      <FormProvider>
+        <BrowserRouter>
+          <RouterConfig />
+        </BrowserRouter>
+      </FormProvider>
     </AuthProvider>
   );
 }
---
+++ src/contexts/formContext.jsx
@@ -0,0 +1,22 @@
+/* eslint-disable react/prop-types */
+import React, { useState } from 'react';
+
+import { useForm } from 'react-hook-form';
+
+import { yupResolver } from '@hookform/resolvers/yup';
+
+const FormContext = React.createContext();
+
+export const FormProvider = ({ children }) => {
+  const [schema, setSchema] = useState(null);
+  const { register, handleSubmit, formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const value = { register, handleSubmit, errors, setSchema };
+
+  return <FormContext.Provider value={value}>{children}</FormContext.Provider>;
+};
+
+export const useFormContext = () => React.useContext(FormContext);
