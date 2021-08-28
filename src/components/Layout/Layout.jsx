bb514db90019304c84383aa40fbdd1c15c30a961
kahasan <kahraman.h@outlook.com>
2021-08-29 02:01:29 +0300

    feat: add basic style for responsive

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 import React from 'react';
 import PropTypes from 'prop-types';
 
@@ -14,33 +16,35 @@ export function Layout({
   console.log('children', typeof children);
   return (
     <Flex data-testid="layout">
-      <Flex h="760px" overflowY="scroll" w="half" px={32.5} flexDir="column" pt={32.5} pb={48}>
+      <Flex h="760px" overflowY="scroll" w={{ base: "full", lg: 'half' }} px={{ base: "20px", md: "140px", lg: "32.5" }} flexDir="column" pt={32.5} pb={48}>
         <Flex mb={5} flexDir="column">
           <Box onClick={() => history.push({ pathname: '/' })} cursor="pointer">
             <Image src="./assets/images/logo.png" w="3xs" />
           </Box>
 
           {title && (
-          <Box mt={10}>
-            {title}
-          </Box>
+            <Box mt={10}>
+              {title}
+            </Box>
           )}
 
           {helperText && (
-          <Box mt={10}>
-            {helperText}
-          </Box>
+            <Box mt={10}>
+              {helperText}
+            </Box>
           )}
 
         </Flex>
         <Flex flexDir="column">{children}</Flex>
       </Flex>
-      <Flex w="half">
+
+      <Flex display={{ base: "none", lg: 'block' }} w="half">
         <Cover imgUrl={imgUrl} />
       </Flex>
     </Flex>
   );
 }
+
 
 Layout.defaultProps = {
   helperText: '',
