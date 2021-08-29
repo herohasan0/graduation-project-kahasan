b22256a7a48afd48dbf5e50d20acff321337c649
kahasan <kahraman.h@outlook.com>
2021-08-30 01:30:54 +0300

    refactor: use form context

--- src/components/AdminResponseForm/AdminResponseForm.jsx
+++ src/components/AdminResponseForm/AdminResponseForm.jsx
@@ -1,30 +1,31 @@
-/* eslint-disable no-undef */
-/* eslint-disable no-alert */
-import React, { useState } from 'react';
+/* eslint-disable no-unused-vars */
+import React, { useState, useEffect } from 'react';
 
-import {
-  Flex, Box,
-} from '@chakra-ui/react';
+import { Flex, Box } from '@chakra-ui/react';
 
 import { useHistory, useLocation } from 'react-router-dom';
 
-import { useForm } from 'react-hook-form';
-
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from 'yup';
 import { FormTextArea, FormSelect, MyButton } from 'components';
 import { updateFormSubmit } from 'helpers/Submit';
 
-const schema = yup.object().shape({
-  response: yup.string(),
-  status: yup.string().required(),
-});
+import { adminResponseFormSchema } from 'helpers/YupSchema';
+
+import { useFormContext } from 'contexts/formContext';
 
 export function AdminResponseForm() {
   const [isLoading, setIsLoading] = useState(false);
-  const { register, handleSubmit, formState: { errors } } = useForm({
-    resolver: yupResolver(schema),
-  });
+
+  const { errors, handleSubmit, register, setSchema } = useFormContext();
+
+  useEffect(() => {
+    let isActive = true;
+    if (isActive) {
+      setSchema(adminResponseFormSchema);
+    }
+    return () => {
+      isActive = false;
+    };
+  }, []);
 
   const history = useHistory();
   const { state } = useLocation();
@@ -39,20 +40,29 @@ export function AdminResponseForm() {
   return (
     <Box w="full" mb="80px" data-testid="admin-response-form">
       <form onSubmit={handleSubmit(onSubmit)}>
-
         <Flex flexDir="column">
-          <FormTextArea label="response" inputTitle="Response" register={register} placeholder="You can send notes to applicant. (optional)" />
-          <FormSelect label="status" inputTitle="Status" register={register} errors={errors} required>
+          <FormTextArea
+            label="response"
+            inputTitle="Admin Note"
+            register={register}
+            placeholder="You can send notes to applicant. (optional)"
+            value={state.result.adminNoted}
+          />
+          <FormSelect
+            label="status"
+            inputTitle="Status"
+            register={register}
+            errors={errors}
+            required
+          >
             <option value="Pending">Pending</option>
             <option value="Approved">Approved</option>
             <option value="Cancelled">Cancelled</option>
           </FormSelect>
 
           <MyButton isLoading={isLoading} text="Send" loadingText="Sending" />
-
         </Flex>
       </form>
     </Box>
-
   );
 }
