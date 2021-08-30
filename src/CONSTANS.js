2eef99230390c748ff0e6db08fc70a77c1d571af
kahasan <kahraman.h@outlook.com>
2021-08-30 14:41:04 +0300

    fix: edit pıblic folder

--- src/CONSTANS.js
+++ src/CONSTANS.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 const COVERURL =
   'https://images.unsplash.com/photo-1590732488817-d34f0d6237a2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80 ';
 
